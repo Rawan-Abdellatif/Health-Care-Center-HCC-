93d50a0bc81c2e227eae09325ee55073acc245ae
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-04-06 12:36:34 -0700

    Add App and Index

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,62 @@
-function App() {
-  return <main>Hello World</main>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Homepage from "./component/Homepage";
+import Header from "./component/Header";
+import GlobalStyle from "./GlobalStyle";
+import Login from "./component/Login";
+import Services from "./component/Services";
+import Contact from "./component/Contact";
+import Info from "./Info";
+import WorkingHours from "./component/WorkingHours";
+import Covid from "./component/Covid";
+import OurStaff from "./component/OurStaff";
+import Footer from "./component/Footer";
+import PatientSignin from "./component/PatientSignin";
+import DoctorSignin from "./component/DoctorSignin";
+import PatientProfile from "./component/PatientProfile";
+import DrProfile from "./component/DrProfile";
+import AppointmentForm from "./component/AppointmentForm";
+import SignupForm from "./component/SingUpForm";
+import ConfirmationSignUp from "./component/ConfirmationSignUp";
+import ConfirmationBook from "./component/ConfirmationBook";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/working-hours" element={<WorkingHours />} />
+          <Route path="/our-staff" element={<OurStaff />} />
+          <Route path="/covid-19" element={<Covid />} />
+          <Route path="/login/patientsignin" element={<PatientSignin />} />
+          <Route path="/login/doctorsignin" element={<DoctorSignin />} />
+          <Route path="/login/doctorsignin/:doctorId" element={<DrProfile />} />
+          <Route
+            path="/login/patientsignin/:patientId"
+            element={<PatientProfile />}
+          />
+          <Route
+            path="/patients/:patientId/appointment"
+            element={<AppointmentForm />}
+          />
+          <Route path="/SignUp" element={<SignupForm />} />
+          <Route path="/ConfirmationSignUp" element={<ConfirmationSignUp />} />
+          <Route
+            path="/confirmationBook/:appointmentId"
+            element={<ConfirmationBook />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,32 +3,74 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
-const port = 8888;
+const cors = require("cors");
+const port = 9999;
 const app = express();
+const { getPatients } = require("./getPatients");
+const { postPatients } = require("./postPatients");
+const { getPatient } = require("./getPatient");
+const { getDoctors } = require("./getDoctors");
+const { getDoctor } = require("./getDoctor");
+const { createAppointment } = require("./createAppointment");
+const { drSignin } = require("./drSignin");
+const { patientSignin } = require("./patientSignin");
+const { getAppointment } = require("./getAppointment");
+const { getAppointments } = require("./getAppointments");
+const { updatePatient } = require("./updatePatient");
+const { deletePatient } = require("./deletePatient");
+// const { patientSignout } = require("./patientSignout");
 // Below are methods that are included in express(). We chain them for convenience.
 //
+app
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan("tiny"))
+  .use(express.json())
+  .use(cors())
+  // Any requests for static files will go into the public folder
+  .use(express.static("public"))
 
-// This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
-app.use(morgan("tiny"));
-app.use(express.json());
+  //
+  // ---------------------------------
+  // add new endpoints here 👇
+  // Get All Of Patients
+  .get("/api/patients", getPatients)
+  //Creat New Patient
+  .post("/api/patient", postPatients)
+  //Get Specific Patient
+  .get("/api/patients/:patientId", getPatient)
+  //Creat new Patient
+  .post("/api/patient", postPatients)
+  //updatePatient
+  .put("/api/patients/:patientId", updatePatient)
+  //Delete Patient
+  .delete("/api/patients/:patientId", deletePatient)
+  //Get All Of Doctors
+  .get("/api/doctors", getDoctors)
+  //Get Specific Doctor
+  .get("/api/doctors/:doctorId", getDoctor)
+  //Creat Appointment
+  .post("/api/appointments", createAppointment)
+  //Login for Doctors
+  .post("/api/doctor/signin", drSignin)
+  //Login for Patients
+  .post("/api/patient/signin", patientSignin)
+  //logOut for patient
+  // .post("/api/patients/signout", patientSignout)
+  //Get Specific Appointment
+  .get("/api/appointment/:appointmentId", getAppointment)
+  //Get Appointments
+  .get("/api/appointments", getAppointments)
 
-// Any requests for static files will go into the public folder
-app.use(express.static("public"));
+  // add new endpoints here ☝️
+  // ---------------------------------
+  //
 
-//
-// ---------------------------------
-// add new endpoints here 👇
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
 
-// add new endpoints here ☝️
-// ---------------------------------
-//
-
-// this is our catch all endpoint.
-app.get("*", (req, res) => {
-  res.status(404).json({
-    status: 404,
-    message: "This is obviously not what you are looking for.",
-  });
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  .listen(port, () => console.log(`Listening on port ${port}`));
