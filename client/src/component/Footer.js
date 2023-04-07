769bc104ec7a3f29cc7d81fff7b08398fd0d7422
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-04-07 10:01:39 -0700

    change styling for footer

---
+++ client/src/component/Footer.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { HiLocationMarker } from "react-icons/hi";
+import { FaPhoneAlt } from "react-icons/fa";
+import { AiFillFacebook } from "react-icons/ai";
+import { ImInstagram } from "react-icons/im";
+const Footer = () => {
+  return (
+    <>
+      <Container>
+        <Div>
+          <div>HCC Health Care Center</div>
+          <div>
+            <HiLocationMarker />
+            3222-Cote des Neige
+          </div>
+          <div>
+            <FaPhoneAlt /> (438)111 333
+          </div>
+        </Div>
+        <Div>
+          <div>Foundation</div>
+          <div>Donate</div>
+          <div>Friends of HCC</div>
+        </Div>
+        <Div>
+          <div>Contact</div>
+          <div>
+            <AiFillFacebook />
+          </div>
+          <div>
+            <ImInstagram />
+          </div>
+        </Div>
+      </Container>
+    </>
+  );
+};
+export default Footer;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* margin-left: 250px; */
+  font-size: 20px;
+  background-color: #c7522a;
+  width: 100%;
+`;
+const Div = styled.div`
+  color: white;
+  margin-top: 35px;
+  height: 220px;
+  display: flex;
+  flex-direction: column;
+  padding-left: 240px;
+`;
