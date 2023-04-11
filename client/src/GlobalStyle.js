3f743a4f4e3852f945a4b12301591b853455c7b6
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-04-11 10:37:11 -0700

    change the color of background

---
+++ client/src/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    background-color:#caffbf;
+    height: 100%;
+    margin: 0;
+  }
+`;
+export default GlobalStyle;
