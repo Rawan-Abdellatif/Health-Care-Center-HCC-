5eae3ade9494a1305f16d0bdeadb312aee011b12
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-04-11 10:38:23 -0700

    add contact page

---
+++ client/src/component/Contact.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const Contact = () => {
+  return (
+    <>
+      <Conatiner>
+        <Main>Our offices </Main>
+        <Div>
+          {" "}
+          1530 Avenu Dr.penfield, Montreal ,(Quebec),H3G 1C1telephone: 438
+          833568
+        </Div>
+        <Div>
+          Get in touch with the HCC team!Please refrain from sharing any
+          personal, medical information with our administrative staff. Details
+          from your personal conditions are not necessary for administrative
+          purposes and should only be shared through the proper channels with
+          your healthcare professionals.
+        </Div>{" "}
+        <Main>www.health@centerHCC.com</Main>
+      </Conatiner>
+    </>
+  );
+};
+export default Contact;
+const Conatiner = styled.div`
+  color: #007f4e;
+  text-align: center;
+  margin-top: 150px;
+  margin-bottom: 100px;
+`;
+const Main = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
+const Div = styled.div`
+  font-size: 18px;
+  padding: 10px;
+`;
